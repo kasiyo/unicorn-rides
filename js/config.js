8eb8a8c136e14006d0bfadae577607b02120fae9
kasiyo <kaiasiripanyo@gmail.com>
2022-12-04 21:50:15 -0600

    Update config.js
    added config data to file

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_7XJi9k0bC',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '8tqdo0ds6rb2jf6un25jnou4b',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
